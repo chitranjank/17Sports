9276c82c3bb1187abe0b9d7a16c3571bf7267e3f
mebusw <mebusw@163.com>
2013-04-05 15:00:59 +0800

    add a map view as popover 

---
+++ SocialMap/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  SocialMap
+//
+//  Created by mebusw on 13-4-5.
+//  Copyright (c) 2013年 mebusw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ SocialMap/MapViewController.m
@@ -0,0 +1,13 @@
+//
+//  MapViewController.m
+//  SocialMap
+//
+//  Created by mebusw on 13-4-5.
+//  Copyright (c) 2013年 mebusw. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@implementation MapViewController
+
+@end
